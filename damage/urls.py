eea69be1e98eafdbf0ffa5a5e0b9a0187c2f554f
calypso15 <ryanjo@gmail.com>
2020-01-25 17:03:28 -0500

    Added compare url

--- damage/urls.py
+++ damage/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
 	# ex: /
     path('', views.index, name='index'),
     
+    # ex: /detail/5/
+    path('detail/<int:weapon_id>/', views.detail, name='detail'),
+    
     # ex: /5/results/
-    path('<int:weapon_id>/', views.detail, name='detail'),
+    path('compare/<int:weapon_id_1>/<int:weapon_id_2>/', views.compare, name='compare'),
 ]
--- damage/views.py
+++ damage/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 
-
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world.")
     
 def detail(request, weapon_id):
-    return HttpResponse("You're looking at weapon %s." % weapon_id)
+    return HttpResponse(f"You're looking at weapon { weapon_id }.")
+    
+def compare(request, weapon_id_1, weapon_id_2):
+    return HttpResponse(f"You're are comparing weapons { weapon_id_1 } and { weapon_id_2 }.")
