5145060a23c0bdbc2a566be36afb09cc1538c833
calypso15 <ryanjo@gmail.com>
2020-01-26 11:36:05 -0500

    Added a slightly more complete detail view

--- damage/views.py
+++ damage/views.py
@@ -1,10 +1,22 @@
+from .models import Weapon
+
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world.")
-    
+	return HttpResponse("Hello, world.")
+	
 def detail(request, weapon_id):
-    return HttpResponse(f"You're looking at weapon { weapon_id }.")
-    
+	results = Weapon.objects.filter(id=weapon_id)
+	
+	if(len(results) == 0):
+		return HttpResponse("No matches.")
+	
+	if(len(results) > 1):
+		return HttpResponse("Multiple matches.")
+		
+	weapon = results[0]
+	
+	return HttpResponse(f"{ weapon.name } (level { weapon.level }). { weapon.damage * weapon.speed } DPS.")
+	
 def compare(request, weapon_id_1, weapon_id_2):
-    return HttpResponse(f"You're are comparing weapons { weapon_id_1 } and { weapon_id_2 }.")
+	return HttpResponse(f"You're are comparing weapons { weapon_id_1 } and { weapon_id_2 }.")
