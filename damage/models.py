a16ce9d80a289178190b3d627ce6ab89740b09fe
calypso15 <ryanjo@gmail.com>
2020-02-08 14:43:49 -0500

    Switched to Postgresql database, updated model

--- damage/models.py
+++ damage/models.py
@@ -1,35 +1,85 @@
 from django.db import models
 
 class Weapon(models.Model):
-	AUTOMATIC_PISTOL = 'automatic pistol'
-	NON_AUTOMATIC_PISTOL = 'non-automatic pistol'
-	SHOTGUN = 'shotgun'
-	AUTOMATIC_RIFLE = 'automatic rifle'
-	NON_AUTOMATIC_RIFLE = 'non-automatic rifle'
-	NON_EXPLOSIVE_HEAVY_GUN = 'non-explosive heavy gun'
-	EXPLOSIVE_HEAVY_GUN = 'explosive heavy gun'
-	ONE_HANDED_MELEE = 'one-handed melee'
-	TWO_HANDED_MELEE = 'two-handed melee'
+	TYPE_PISTOL = 'pistol'
+	TYPE_SHOTGUN = 'shotgun'
+	TYPE_RIFLE = 'rifle'
+	TYPE_HEAVY = 'heavy gun'
+	TYPE_MELEE = 'melee'
+	TYPE_THROWN = 'thrown'
 	
 	WEAPON_TYPES = (
-		(AUTOMATIC_PISTOL, 'Automatic Pistol'),
-		(NON_AUTOMATIC_PISTOL, 'Non-Automatic Pistol'),
-		(SHOTGUN, 'Shotgun'),
-		(AUTOMATIC_RIFLE, 'Automatic Rifle'),
-		(NON_AUTOMATIC_RIFLE, 'Non-Automatic Rifle'),
-		(NON_EXPLOSIVE_HEAVY_GUN, 'Non-Explosive Heavy Gun'),
-		(EXPLOSIVE_HEAVY_GUN, 'Explosive Heavy Gun'),
-		(ONE_HANDED_MELEE, 'One-Handed Melee'),
-		(TWO_HANDED_MELEE, 'Two-Handed Melee'),
+		(TYPE_PISTOL, 'Pistol'),
+		(TYPE_SHOTGUN, 'Shotgun'),
+		(TYPE_RIFLE, 'Rifle'),
+		(TYPE_HEAVY, 'Heavy Gun'),
+		(TYPE_MELEE, 'Melee'),
+		(TYPE_THROWN, 'Thrown'),
+	)
+	
+	AMMO_NONE = 'none'
+	AMMO_308 = '.308'
+	AMMO_38 = '.38'
+	AMMO_44 = '.44'
+	AMMO_45 = '.45'
+	AMMO_50BALL = '.50 ball'
+	AMMO_50 = '.50'
+	AMMO_10MM = '10mm'
+	AMMO_556 = '5.56mm'
+	AMMO_5MM = '5mm'
+	AMMO_CANNONBALL = 'cannonball'
+	AMMO_SPIKE = 'railway spike'
+	AMMO_SHELL = 'shotgun shell'
+	AMMO_2MMEC = '2mm ec'
+	AMMO_ALIEN = 'alien blaster'
+	AMMO_CRYO = 'cryo cell'
+	AMMO_FUEL = 'fuel'
+	AMMO_FUSIONCELL = 'fusion cell'
+	AMMO_FUSIONCORE = 'fusion core'
+	AMMO_GAMMA = 'gamma'
+	AMMO_PLASMA = 'plasma cartridge'
+	AMMO_PLASMACORE = 'plasma core'
+	AMMO_40MMGRENADE = '40mm grenade'
+	AMMO_MININUKE = 'mini nuke'
+	AMMO_MISSILE = 'missile'
+	
+	AMMO_TYPES = (
+		(AMMO_NONE, 'None'),
+		(AMMO_308, '.308'),
+		(AMMO_38, '.38'),
+		(AMMO_44, '.44'),
+		(AMMO_45, '.45'),
+		(AMMO_50BALL, '.50 ball'),
+		(AMMO_50, '.50'),
+		(AMMO_10MM, '10mm'),
+		(AMMO_556, '5.56mm'),
+		(AMMO_5MM, '5mm'),
+		(AMMO_CANNONBALL, 'Cannonball'),
+		(AMMO_SPIKE, 'Railway Spike'),
+		(AMMO_SHELL, 'Shotgun Shell'),
+		(AMMO_2MMEC, '2mm EC'),
+		(AMMO_ALIEN, 'Alien Blaster'),
+		(AMMO_CRYO, 'Cryo Cell'),
+		(AMMO_FUEL, 'Fuel'),
+		(AMMO_FUSIONCELL, 'Fusion Cell'),
+		(AMMO_FUSIONCORE, 'Fusion Core'),
+		(AMMO_GAMMA, 'Gamma'),
+		(AMMO_PLASMA, 'Plasma Cartridge'),
+		(AMMO_PLASMACORE, 'Plasma Core'),
+		(AMMO_40MMGRENADE, '40mm Grenade'),
+		(AMMO_MININUKE, 'Mini Nuke'),
+		(AMMO_MISSILE, 'Missile'),
 	)
 
 	name = models.CharField(max_length=64)
 	type = models.CharField(max_length=32, choices=WEAPON_TYPES)
-	energy = models.BooleanField(default=False)
-	strength_factor = models.DecimalField(max_digits=3, decimal_places=2)
-	speed = models.DecimalField(max_digits=4, decimal_places=2)
 	level = models.PositiveSmallIntegerField()
 	damage = models.PositiveSmallIntegerField()
-	
+	ammo = models.CharField(max_length=32, choices=AMMO_TYPES)
+	automatic = models.BooleanField(default=False)
+	explosive = models.BooleanField(default=False)
+	speed = models.DecimalField(max_digits=4, decimal_places=2)
+	strength_factor = models.DecimalField(max_digits=3, decimal_places=2)
+
 	def __str__(self):
 		return f"{self.name} (level {self.level})"
--- mysite/settings.py
+++ mysite/settings.py
@@ -75,9 +75,18 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'fallout',
+        'USER': 'fallout76',
+        'PASSWORD': 'fallout76',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
